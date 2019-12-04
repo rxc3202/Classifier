d37465e94a13dd85de6b5eeb34e7ece56bf7b1a1
Ryan Cervnates <rxc3202@rit.edu>
2019-12-04 15:36:30 -0500

    fixed testing file

--- tests/test_dt.py
+++ tests/test_dt.py
@@ -14,7 +14,7 @@ from decisiontree import DecisionTree
 from adaboost import Adaboost
 
 def usage():
-    print("test_dt.py <ssize|depth> <examples> <hypothesisOut> <learning-type> <testing-set>")
+    print("test_dt.py <ssize|depth> <training-set> <hypothesisOut> <testing-set>")
 
 def process_file(filename, training=True):
     raw_text = []
@@ -33,11 +33,7 @@ def process_file(filename, training=True):
 def handle_train(argv, size=None, depth=None):
     examples = process_file(argv[2], training=True)
     tree = None
-    if argv[4] == "dt":
-        tree = DecisionTree()
-    else:
-        tree = Adaboost()
-    
+    tree = DecisionTree()
     tree.define_positive_class(lambda x: x.classification == 'en')
     tree.define_classes(processing.classes)
     tree.define_attributes(processing.attr_definitions)
@@ -50,7 +46,7 @@ def handle_train(argv, size=None, depth=None):
     else:
         tree.generate(tree.examples)
     with open(argv[3], "w") as f:
-        f.write(argv[4] + "\n")
+        f.write("dt" + "\n")
         f.write(str(tree.tree))
     f.close()
     tree.print()
@@ -66,54 +62,43 @@ def handle_predict(argv):
     f.close()
     hypothesis = literal_eval(hypothesis)
     tree = None
-    if model == "dt":
-        tree = DecisionTree()
-    else:
-        tree = Adaboost()
+    tree = DecisionTree()
     tree.define_positive_class(lambda x: x.classification == 'en')
     tree.define_classes(processing.classes)
     tree.define_attributes(processing.attr_definitions)
-    examples = process_file(argv[5], training=False)
+    examples = process_file(argv[4], training=False)
     examples = tree.create_examples(examples)
     return tree.classify(examples, hypothesis)
 
 
 def main():
-    if len(sys.argv) != 6:
+    def predict():
+        results = handle_predict(sys.argv)
+        dutch = 0
+        english = 0
+        for i in range(50):
+            if i < 25 and results[i] != 'nl':
+                dutch += 1
+                continue
+            if i >= 25 and results[i] != 'en':
+                english += 1
+        print(results)
+        print(f"Dutch Error: %{dutch/25*100}")
+        print(f"English Error: %{english/25*100}")
+
+    if len(sys.argv) != 5:
         usage()
         sys.exit(1)
     if sys.argv[1] == "ssize":
         for size in range(10, 101, 10):
             print(f"===================== Sample Size: {size} =====================")
             handle_train(sys.argv, size)
-            results = handle_predict(sys.argv)
-            dutch = 0
-            english = 0
-            for i in range(20):
-                if i < 25 and results[i] != 'nl':
-                    dutch += 1
-                    continue
-                if i >= 25 and results[i] != 'en':
-                    english += 1
-            #print(results)
-            print(f"Dutch Error: %{dutch/25*100}")
-            print(f"English Error: %{english/25*100}")
+            predict()
     else:
         for i in range(10):
             print(f"===================== Depth: {i+1} =====================")
             handle_train(sys.argv, depth=i)
-            results = handle_predict(sys.argv)
-            dutch = 0
-            english = 0
-            for i in range(50):
-                if i < 25 and results[i] != 'nl':
-                    dutch += 1
-                    continue
-                if i >= 25 and results[i] != 'en':
-                    english += 1
-            #print(results)
-            print(f"Dutch Error: %{dutch/25*100}")
-            print(f"English Error: %{english/25*100}")
+            predict()
 
 
 if __name__ == '__main__':
