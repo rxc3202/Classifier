2664821e298846fb5de3abca3dc606696ca7e71b
Ryan Cervnates <rxc3202@rit.edu>
2019-12-04 12:05:27 -0500

    updated training/testing set; added tests

---
+++ tests/test_dt.py
@@ -0,0 +1,120 @@
+#! /usr/bin/python3
+"""
+Author: Ryan Cervantes (rxc3202)
+Date: 11 November 2019
+Filename: test_dt.py
+"""
+import sys
+# used for testing
+sys.path.append("..")
+import re
+from ast import literal_eval
+import test_processing as processing
+from decisiontree import DecisionTree
+from adaboost import Adaboost
+
+def usage():
+    print("test_dt.py <ssize|depth> <examples> <hypothesisOut> <learning-type> <testing-set>")
+
+def process_file(filename, training=True):
+    raw_text = []
+    with open(filename, 'r') as f:
+        raw_text = [re.sub("[^(a-zA-Z\d\-\s\|)]",'', line).strip("\n ").lower() for line in f]
+    labeled_examples = []
+    for example in raw_text:
+        example = example.split("|")
+        if training:
+            labeled_examples.append(tuple([f(example[1]) for f in processing.processing_funcs] + [example[0]]))
+        else:
+            labeled_examples.append(tuple([f(example[0]) for f in processing.processing_funcs] + [None]))
+    return labeled_examples
+
+
+def handle_train(argv, size=None, depth=None):
+    examples = process_file(argv[2], training=True)
+    tree = None
+    if argv[4] == "dt":
+        tree = DecisionTree()
+    else:
+        tree = Adaboost()
+    
+    tree.define_positive_class(lambda x: x.classification == 'en')
+    tree.define_classes(processing.classes)
+    tree.define_attributes(processing.attr_definitions)
+    if size:
+        tree.load_examples(examples[:size])
+    else:
+        tree.load_examples(examples)
+    if depth != None:
+        tree.generate(tree.examples, depth)
+    else:
+        tree.generate(tree.examples)
+    with open(argv[3], "w") as f:
+        f.write(argv[4] + "\n")
+        f.write(str(tree.tree))
+    f.close()
+    tree.print()
+
+
+def handle_predict(argv):
+    hypothesis = None
+    model = None
+    with open(argv[3], "r") as f:
+        # DONT DO THIS ITS INSECURE. IM INSANE
+        model = f.readline().strip('\n')
+        hypothesis = f.readline()
+    f.close()
+    hypothesis = literal_eval(hypothesis)
+    tree = None
+    if model == "dt":
+        tree = DecisionTree()
+    else:
+        tree = Adaboost()
+    tree.define_positive_class(lambda x: x.classification == 'en')
+    tree.define_classes(processing.classes)
+    tree.define_attributes(processing.attr_definitions)
+    examples = process_file(argv[5], training=False)
+    examples = tree.create_examples(examples)
+    return tree.classify(examples, hypothesis)
+
+
+def main():
+    if len(sys.argv) != 6:
+        usage()
+        sys.exit(1)
+    if sys.argv[1] == "ssize":
+        for size in range(10, 101, 10):
+            print(f"===================== Sample Size: {size} =====================")
+            handle_train(sys.argv, size)
+            results = handle_predict(sys.argv)
+            dutch = 0
+            english = 0
+            for i in range(20):
+                if i < 25 and results[i] != 'nl':
+                    dutch += 1
+                    continue
+                if i >= 25 and results[i] != 'en':
+                    english += 1
+            #print(results)
+            print(f"Dutch Error: %{dutch/25*100}")
+            print(f"English Error: %{english/25*100}")
+    else:
+        for i in range(10):
+            print(f"===================== Depth: {i+1} =====================")
+            handle_train(sys.argv, depth=i)
+            results = handle_predict(sys.argv)
+            dutch = 0
+            english = 0
+            for i in range(50):
+                if i < 25 and results[i] != 'nl':
+                    dutch += 1
+                    continue
+                if i >= 25 and results[i] != 'en':
+                    english += 1
+            #print(results)
+            print(f"Dutch Error: %{dutch/25*100}")
+            print(f"English Error: %{english/25*100}")
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_processing.py
@@ -0,0 +1,138 @@
+#! /usr/bin/python3
+"""
+Filename: processing.py
+"""
+
+
+def has_aa(text):
+    text = text.split(' ')
+    for word in text:
+        if "aa" in word:
+            return True
+    return False
+
+def has_the(text):
+    text = text.split(' ')
+    for word in text:
+        if word == "the":
+            return True
+    return False
+
+
+def has_jn(text):
+    text = text.split(' ')
+    for word in text:
+        if "jn" in word:
+            return True
+    return False
+
+
+def has_de(text):
+    text = text.split(' ')
+    for word in text:
+        if "de" == word:
+            return True
+    return False
+
+
+def has_van(text):
+    text = text.split(' ')
+    for word in text:
+        if "van" == word:
+            return True
+    return False
+
+
+def has_a(text):
+    text = text.split(' ')
+    for word in text:
+        if "a" == word:
+            return True
+    return False
+
+
+def has_jk(text):
+    text = text.split(' ')
+    for word in text:
+        if "jk" in word:
+            return True
+    return False
+
+
+def ends_en(text):
+    text = text.split(' ')
+    for word in text:
+        if word[-2:] == "en":
+            return True
+    return False
+
+
+def has_uu(text):
+    text = text.split(' ')
+    for word in text:
+        if "uu" in word:
+            return True
+    return False
+
+
+def has_oo(text):
+    text = text.split(' ')
+    for word in text:
+        if "oo" in word:
+            return True
+    return False
+
+
+def has_of(text):
+    text = text.split(' ')
+    for word in text:
+        if "of" == word:
+            return True
+    return False
+
+def has_ee(text):
+    text = text.split(' ')
+    for word in text:
+        if "ee" in word:
+            return True
+    return False
+
+def has_oo(text):
+    text = text.split(' ')
+    for word in text:
+        if "oo" in word:
+            return True
+    return False
+
+#icht ending
+
+processing_funcs = [
+    has_aa,
+    has_uu,
+    has_the,
+    has_jn,
+    has_de,
+    has_van,
+    has_a,
+    ends_en,
+    has_oo,
+    has_of,
+    has_ee,
+
+]
+
+classes = ['en', 'nl']
+attr_definitions = [
+        ("aa", True, False),
+        ("uu", True, False),
+        ("the", True, False),
+        ("jn", True, False),
+        ("de", True, False),
+        ("van", True, False),
+        ("a", True, False),
+        ("en_ending", True, False),
+        ("oo", True, False),
+        ("of", True, False),
+        ("ee", True, False)
+]
+attr_names = [x[0] for x in attr_definitions]
